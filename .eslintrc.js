3f00a06b279f9e31034582ab731ec801974093fa
Jack Domleo <jackdomleo7@gmail.com>
2021-04-24 12:23:30 +0100

    Setup linting for unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    'jest/globals': true
   },
+  plugins: ['eslint-plugin-jest'],
   parserOptions: {
     parser: '@typescript-eslint/parser'
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:jest/all'
   ],
   // add your custom rules here
   rules: {
     'vue/no-v-html': 'off',
     'vue/singleline-html-element-content-newline': ['warn', {
       ignores: ['pre', 'textarea', 'nuxt-link']
-    }]
+    }],
+    'jest/lowercase-name': 'off',
+    'jest/prefer-expect-assertions': 'off'
   }
 }
