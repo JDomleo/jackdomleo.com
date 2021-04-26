eb517c40c497d361ba846aa9d73f3cf84afa68d6
Jack Domleo <jackdomleo7@gmail.com>
2021-04-26 08:55:20 +0100

    jest-axe example

--- tests/unit/components/Debug.spec.ts
+++ tests/unit/components/Debug.spec.ts
@@ -1,5 +1,15 @@
 import { mount, shallowMount } from '@vue/test-utils'
+import { configureAxe, toHaveNoViolations } from 'jest-axe'
+
 import Debug from '@/components/Debug.vue'
+
+expect.extend(toHaveNoViolations)
+
+const axe = configureAxe({
+  rules: {
+    region: { enabled: false } // Don't need to check because this component is isolated
+  }
+})
 
 describe('Debug.vue component', () => {
   /**
@@ -44,5 +54,21 @@ describe('Debug.vue component', () => {
     })
 
     expect(wrapper.element).toMatchSnapshot()
+  })
+
+  /**
+   * A11y
+   */
+
+   it('to have no obvious accessibility violations', async () => {
+    const wrapper = mount(Debug, {
+      mocks: {
+        $colorMode: {
+          preference: 'system'
+        }
+      }
+    })
+
+    expect(await axe(wrapper.element)).toHaveNoViolations()
   })
 })
