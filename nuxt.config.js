2a526b4570b322491fd6cab44a6c8371977b22d8
Jack Domleo <jackdomleo7@gmail.com>
2021-05-08 09:41:17 +0100

    Fixed routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,7 +80,7 @@ export default {
       const { $content } = require('@nuxt/content')
       const files = await $content({ deep: true }).only(['path']).fetch()
 
-      return files.map(file => file.path)
+      return files.map(file => `/blog/${file.path}`)
     }
   }
 }
